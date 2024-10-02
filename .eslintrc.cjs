3d59ab716bdeb806368ceb96d011e063cfa4e561
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-10-01 23:23:15 -0500

    eslint issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,31 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended', // Add React plugin
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true, // Enable JSX parsing
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react-refresh', 'react'], // Add React plugin here
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react-hooks/rules-of-hooks': 'error', // Ensure hooks are used correctly
+    'react-hooks/exhaustive-deps': 'warn', // Warn if missing dependencies in hooks
+    'react/react-in-jsx-scope': 'off', // Turn off this rule since React 17+ doesn't require importing React in every file
   },
-}
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the version of React
+    },
+  },
+};
