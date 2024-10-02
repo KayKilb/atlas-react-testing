23ed9e77a5b1f8ba1305e8a99bc3ea16a229f598
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-10-01 22:52:41 -0500

    adding jsdom, errors for vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/__tests__/**/*.test.tsx'],
+  },
 });
