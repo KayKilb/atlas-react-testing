94ddd488612770c8f43201787bb0690616c2d45a
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-10-01 23:14:12 -0500

    vitest global again

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+// Importing Vitest config helper
+import { defineConfig as defineVitestConfig } from 'vitest/config';
 
-export default defineConfig({
+export default defineVitestConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
-    include: ['src/__tests__/**/*.test.tsx'],
+    include: ['src/__tests__/**/*.test.tsx'], // Define the test files location
   },
 });
